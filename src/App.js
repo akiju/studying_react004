290d5afe933d655e4f06fcaed176a9adeb87658f
akiju <99388105+akiju@users.noreply.github.com>
2022-03-18 18:35:40 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { ChileArea } from "./ChileArea";
+import "./styles.css";
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChileArea open={open} />
+    </div>
+  );
+}
---
+++ src/ChileArea.jsx
@@ -0,0 +1,27 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChileArea = (props) => {
+  const { open } = props;
+
+  const data = [...Array(2000).keys()];
+
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  console.log(data);
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
