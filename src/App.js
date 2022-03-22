77933084fe2dee3103102309faf3eac67712b3e0
akiju <99388105+akiju@users.noreply.github.com>
2022-03-22 15:53:21 +0900

    220322 220322

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "./styles.css";
 
 export default function App() {
   const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
+  const onClickClose = () => setOpen(false);
 
   return (
     <div className="App">
@@ -15,7 +16,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChileArea open={open} />
+      <ChileArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChileArea.jsx
+++ src/ChileArea.jsx
@@ -5,7 +5,7 @@ const style = {
 };
 
 export const ChileArea = (props) => {
-  const { open } = props;
+  const { open, onClickClose } = props;
 
   const data = [...Array(2000).keys()];
 
@@ -20,6 +20,8 @@ export const ChileArea = (props) => {
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
